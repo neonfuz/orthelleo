feb91ddea69af7d274bac0f0a8ce127a5dc9c816
neonfuz <neonfuz@gmail.com>
2018-04-27 14:32:55 -0500

    Added a reasonable default state

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,21 @@
+/* eslint no-sparse-arrays: 0 */
+
+// Piece constants
+const W = 'W', B = 'B';
+export { W, B };
 
 const defaultState = {
+  turn: 'white',
+  board: [
+    [ , , , , , , , ],
+    [ , , , , , , , ],
+    [ , , , , , , , ],
+    [ , , ,W,B, , , ],
+    [ , , ,B,W, , , ],
+    [ , , , , , , , ],
+    [ , , , , , , , ],
+    [ , , , , , , , ],
+  ],
 };
 
 function reducer(state = defaultState, action) {
